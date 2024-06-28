907cae43a16a523e6f0f4ede31ed020c94fbc485
Nguyen Dinh Phuc <hanoi2003.a@gmail.com>
2024-06-28 17:50:56 +0700

    Category scrollbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div></div>;
+  return <div>Hello</div>;
 };
 
 export default Footer;
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,128 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between lg:justify-center flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c0604e24-0b81-43f7-9288-73b8274dda45/air-jordan-4-retro-oxidised-green-shoes-lw9R0z.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/772e294b-14f2-4910-8463-e668b224dda5/air-jordan-4-retro-oxidised-green-shoes-lw9R0z.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$54</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/82fbf807-af52-47ca-96bd-a5fd59dbbbd2/air-jordan-3-retro-shoes-TJf2lm.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/2fd9398f-c605-4cac-855d-a876d2943672/air-jordan-3-retro-shoes-TJf2lm.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$54</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/77c98012-1e1a-4d57-8a69-162af8874e58/jumpman-mvp-shoes-JV1HCs.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/8eb6158a-b4fd-4bb2-8125-ee8a153d2018/jumpman-mvp-shoes-JV1HCs.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$54</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f01d577c-e0c4-4fb9-8e6b-a2dcf294d879/jordan-deja-sandals-3mH5Np.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/1408088f-841b-41cb-808f-cc2f9ab70953/jordan-deja-sandals-3mH5Np.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$54</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
