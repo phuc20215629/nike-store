bb03cfae04a204dd99bfba16ba25abf20d659275
Nguyen Dinh Phuc <hanoi2003.a@gmail.com>
2024-06-28 20:47:00 +0700

    Footer

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -3,144 +3,82 @@ import Link from "next/link";
 
 const CategoryList = () => {
   return (
-    <div className="px-4 overflow-x-scroll">
-      <div className="flex gap-4 md:gap-8">
+    <div className="px-10 overflow-x-scroll h-[600px]">
+      <div className="flex gap-4 md:gap-8 h-[500px]">
         <Link
           href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/3 xl:w-1/3"
         >
-          <div className="relative bg-slate-100 w-full h-96">
+          <div className="relative bg-slate-100 w-full h-full">
             <Image
               src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
               alt="not showing"
               fill
-              sizes="20vw"
+              sizes="30"
               className="object-cover"
             />
           </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
-              alt="not showing"
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
+          <h1 className="mt-6 font-medium tracking-wide ">Category Name</h1>
         </Link>
         <Link
           href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/3 xl:w-1/3"
         >
-          <div className="relative bg-slate-100 w-full h-96">
+          <div className="relative bg-slate-100 w-full h-full">
             <Image
               src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
               alt="not showing"
               fill
-              sizes="20vw"
+              sizes="30"
               className="object-cover"
             />
           </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
+          <h1 className="mt-6 font-medium tracking-wide ">Category Name</h1>
         </Link>
         <Link
           href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/3 xl:w-1/3"
         >
-          <div className="relative bg-slate-100 w-full h-96">
+          <div className="relative bg-slate-100 w-full h-full">
             <Image
               src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
               alt="not showing"
               fill
-              sizes="20vw"
+              sizes="30"
               className="object-cover"
             />
           </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
+          <h1 className="mt-6 font-medium tracking-wide ">Category Name</h1>
         </Link>
         <Link
           href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/3 xl:w-1/3"
         >
-          <div className="relative bg-slate-100 w-full h-96">
+          <div className="relative bg-slate-100 w-full h-full">
             <Image
               src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
               alt="not showing"
               fill
-              sizes="20vw"
+              sizes="30"
               className="object-cover"
             />
           </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
+          <h1 className="mt-6 font-medium tracking-wide ">Category Name</h1>
         </Link>
         <Link
           href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/3 xl:w-1/3"
         >
-          <div className="relative bg-slate-100 w-full h-96">
+          <div className="relative bg-slate-100 w-full h-full">
             <Image
               src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
               alt="not showing"
               fill
-              sizes="20vw"
+              sizes="30"
               className="object-cover"
             />
           </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
-              alt="not showing"
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://static.nike.com/a/images/w_960,c_limit/37d3cd53-38b1-42ab-a2a3-d116ff67d79d/image.jpg"
-              alt="not showing"
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
+          <h1 className="mt-6 font-medium tracking-wide ">Category Name</h1>
         </Link>
       </div>
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaGlobe, FaRegCopyright } from "react-icons/fa";
+
 const Footer = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 text-sm mt-24 border-t-2">
+      <div className="flex flex-col md:flex-row justify-between font-medium">
+        <div className="flex flex-col justify-between">
+          <h1 className="text-xl tracking-wide mb-8">Resources</h1>
+          <div className="flex flex-col gap-6 text-gray-500">
+            <Link href="">Find A Store</Link>
+            <Link href="">Become A Member</Link>
+            <Link href="">Student Discounts</Link>
+            <Link href="">Send Us Feedback</Link>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between">
+          <h1 className="text-xl tracking-wide mb-8">Help</h1>
+          <div className="flex flex-col gap-6 text-gray-500">
+            <Link href="">Get Help</Link>
+            <Link href="">Order Status</Link>
+            <Link href="">Delivery</Link>
+            <Link href="">Contact Us</Link>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between">
+          <h1 className="text-xl tracking-wide mb-8">Company</h1>
+          <div className="flex flex-col gap-6 text-gray-500">
+            <Link href="">About Nike</Link>
+            <Link href="">News</Link>
+            <Link href="">Careers</Link>
+            <Link href="">Investors</Link>
+          </div>
+        </div>
+        <div className="flex flex-row w-1/10 justify-between mx-2">
+          <FaGlobe />
+          <div className="ml-2 -mt-1">Vietnam</div>
+        </div>
+      </div>
+      {/* BOTTOM */}
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="text-gray-500 font-bold">
+            <span className=" mr-4 flex flex-row">
+              <FaRegCopyright />
+              <span className="ml-2 -mt-1">
+                2024 Nike, Inc. All rights reserved
+              </span>
+              <div className=" mr-4 flex flex-row gap-5 ml-5 -mt-1">
+                <Link href="/" className="hover:text-black">
+                  Guides
+                </Link>
+                <Link href="/" className="hover:text-black">
+                  Terms of Sale
+                </Link>
+                <Link href="/" className="hover:text-black">
+                  Terms of Use
+                </Link>
+                <Link href="/" className="hover:text-black">
+                  Nike Privacy Policy
+                </Link>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
